b1e6028d0f1c103fefcff2fbb5c4cd3cae02b47c
wuyaoyao99 <957625489@qq.com>
2020-06-30 13:21:57 +0800

    123

---
+++ venv/new.py
@@ -0,0 +1,6 @@
+print("hello python")
+import mssdk as qh
+qh.set_token
+import sys; print('Python %s on %s' % (sys.version, sys.platform))
+sys.path.extend(['D:\\研二下\\数据工程师实习\\pycharm', 'D:/研二下/数据工程师实习/pycharm'])
+a=123
